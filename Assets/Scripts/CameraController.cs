bae14dd1ba11e3785c374acb3c352ed23658a075
shunhaku518 <ln.m.a.anel@gmail.com>
2025-09-04 15:28:02 +0900

    スクロールの設定まで

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    float x, y, z; //カメラの座標を決めるための変数
+
+    [Header("カメラの限界地")]
+    public float leftLimit;
+    public float rightLimit;
+    public float topLimit;
+    public float bottomLimit;
+
+    [Header("カメラのスクロール設定")]
+    public bool isScrollX; //横方向に強制スクロールするかのフラグ
+    public float scrollSpeeedX = 0.5f;
+    public bool isScrollY;　//横方向に強制スクロールするかのフラグ
+    public float scrollSpeedY = 0.5f;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Playerタグをもったゲームオブジェクトを探して、変数playerに代入
+        player = GameObject.FindGameObjectWithTag("Player");
+        //カメラのZ座標は初期値のままを維持したい
+
+        z = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //いったんプレイヤーのX座標、Y座標の位置を変数に取得
+        x = player.transform.position.x;
+        y = player.transform.position.y;
+
+        //もしも横の強制スクロールフラグが立っていたら
+        if (isScrollX)
+        {
+            //前の座標に変数分だけ加した座標
+            x = transform.position.x + (scrollSpeeedX * Time.deltaTime);
+        }
+
+        //もしも左右の限界までプレイヤーが移動したら
+        if (x < leftLimit)
+        {
+            x = leftLimit;
+        }
+        else if (x>rightLimit)
+        {
+            x = rightLimit;
+        }
+
+        //もしも縦の強制スクロールフラグが立っていたら
+        if (isScrollY)
+        {
+            //前の座標に変数分だけ加した座標
+            y = transform.position.y + (scrollSpeedY * Time.deltaTime);
+        }
+
+        //もしも上下の限界までプレイヤーが移動したら
+        if (y < bottomLimit)
+        {
+            y = bottomLimit;
+        }
+        else if (y>topLimit)
+        {
+            y = topLimit;
+        }
+
+        //取り決めた各変数x,y,zの値をカメラのポジションとする
+        transform.position = new Vector3(x, y, z);
+    }
+}
